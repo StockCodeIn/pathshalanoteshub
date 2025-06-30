32f15c192dc1dad0c1190cb754a29212157d3a24
StockCodeIn <kumarashokkumar2022@gmail.com>
2025-06-30 13:36:49 +0530

    Fixed image imports and moved assets to public folder

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,25 +1,13 @@
-import logo from '@/assets/logo.png';
-import menu from '@/assets/menu.png';
-import search from '@/assets/search.png';
-import close from '@/assets/close.png';
-import Close from '@/assets/close-panel.png';
-import gk from '@/assets/GK.png'
-import cbse from '@/assets/CBSE.png';
-import rbse from '@/assets/RBSE.png';
-import pastPapers from '@/assets/PAPER.png';
-import study from '@/assets/STUDY.png';
-import time from '@/assets/TIME.png';
-
 export const assets = {
-    logo,
-    menu,
-    search,
-    close,
-    Close,
-    gk,
-    cbse,
-    rbse,
-    pastPapers,
-    study,
-    time,
-}
+  logo: '/assets/logo.png',
+  menu: '/assets/menu.png',
+  search: '/assets/search.png',
+  close: '/assets/close.png',
+  Close: '/assets/close-panel.png',
+  gk: '/assets/GK.png',
+  cbse: '/assets/CBSE.png',
+  rbse: '/assets/RBSE.png',
+  pastPapers: '/assets/PAPER.png',
+  study: '/assets/STUDY.png',
+  time: '/assets/TIME.png',
+};
